70b6b55ccbc339d89955793c222e3382c3dc6fbd
Thalita <thachacon@gmail.com>
2024-05-29 18:54:18 -0300

    operadores unarios e operador ternario

---
+++ tipos-variaveis/src/OperadorTernario.java
@@ -0,0 +1,9 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        
+
+
+
+        
+    }
+}
---
+++ tipos-variaveis/src/OperadoresUnarios.java
@@ -0,0 +1,20 @@
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+        
+        int numero = 5;
+
+        numero++; //repetição com incremento.
+                  // numero++ é a mesma coisa que dizer numero = numero + 1;
+        System.out.println(numero);
+
+
+        //BOOLEANO:
+
+        boolean variavel = true;
+
+        variavel = !variavel; //eu declarei a variavel como true e para inverter,
+                                //utilizo o ! 
+
+        System.out.println(variavel);
+    }
+}
