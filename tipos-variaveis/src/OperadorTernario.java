233c0ec236237d39ebaec90af09d43271abd510c
Thalita <thachacon@gmail.com>
2024-06-04 17:39:35 -0300

    operadores relacionais e ternario

--- tipos-variaveis/src/OperadorTernario.java
+++ tipos-variaveis/src/OperadorTernario.java
@@ -1,9 +1,22 @@
 public class OperadorTernario {
     public static void main(String[] args) {
-        
+        int a, b;
+
+        a = 5;
+        b = 6;
 
 
+        /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+        */
 
-        
+        //EXEMPLO UTILIZANDO ? : 
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
     }
 }
---
+++ tipos-variaveis/src/OperadoresRelacionais.java
@@ -0,0 +1,20 @@
+public class OperadoresRelacionais {
+
+    public static void main(String[] args) {
+       
+        //classe Operadores.java
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+        
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+    
+    
+    
+    }
+}
